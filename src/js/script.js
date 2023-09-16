ec4ad20742b336d99e633fe8605036c88e2dea29
miyamotomitsuaki <riochi0822@gmail.com>
2023-09-16 11:11:31 +0900

    修正のコミット2(R5.9.16)

--- src/js/script.js
+++ src/js/script.js
@@ -42,11 +42,11 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     spaceBetween: 24,
     loop: true,
     speed: 800,
-    // autoplay: {
-    // delay: 3000,
+    autoplay: {
+    delay: 3000,
     // 矢印をクリックして自動再生を止める
     disableOnInteraction: true,
-    // },
+    },
 
     breakpoints: {
       // スライドの表示枚数：768px以上の場合
